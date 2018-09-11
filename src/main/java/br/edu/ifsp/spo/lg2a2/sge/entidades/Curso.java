c9f4d5d95704824e03fd9c55352bdbbec4d3c954
AndreyLacerda <andrey-lacerda@hotmail.com>
2018-09-11 08:49:21 -0300

    Exercicio Criar Curso e Aluno

--- src/main/java/br/edu/ifsp/spo/lg2a2/sge/entidades/Curso.java
+++ src/main/java/br/edu/ifsp/spo/lg2a2/sge/entidades/Curso.java
@@ -4,6 +4,11 @@ import java.util.Collection;
 
 public class Curso {
 	
+	public Curso(String nome, String codigo) {
+		this.nome = nome;
+		this.codigo = codigo;
+	}
+	
 	private Collection<Turma> turmas;
 	public Collection<Turma> getTurmas() {
 		return this.turmas;
